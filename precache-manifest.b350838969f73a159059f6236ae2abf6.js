1b72d2e832bda042f61a5a40c8fb21884b7c6fc3
Filianov Pavlo <filianov@Pavlo-MacBook-Pro.local>
2020-03-09 21:36:38 +0200

    Updates

---
+++ precache-manifest.b350838969f73a159059f6236ae2abf6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab7a4b80edefe01ca3d65b5029133efd",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "51f801cd7af0c48bdac2",
+    "url": "/goit-react-hw-01-components/static/css/main.8152b083.chunk.css"
+  },
+  {
+    "revision": "4532e9b1f1018f559743",
+    "url": "/goit-react-hw-01-components/static/js/2.01e7a021.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/goit-react-hw-01-components/static/js/2.01e7a021.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51f801cd7af0c48bdac2",
+    "url": "/goit-react-hw-01-components/static/js/main.530d1003.chunk.js"
+  },
+  {
+    "revision": "a0495ec637e3d6aa9326",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.fa359fff.js"
+  }
+]);
